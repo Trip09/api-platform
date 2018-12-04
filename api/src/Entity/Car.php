2c59ffb49e0f3098b8ac68252485451d11850dcd
Joao Albuquerque <albuquerque.joao.filipe@gmail.com>
2018-12-04 11:45:34 +0000

    Commit 1

---
+++ api/src/Entity/Car.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A Uber car avaialable to rent
+ *
+ * @ORM\Entity
+ * @ApiResource()
+ */
+class Car
+{
+
+    /**
+     * A id for the car
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * A nice brand of the car
+     *
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    public $brand;
+
+    /**
+     *
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    public $model;
+
+    /**
+     *
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    public $plateNumber;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
